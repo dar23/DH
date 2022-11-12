116c5e1855f433907fa7c115a57dbe92ff400d55
Dariusz Hozer <33494247+dar23@users.noreply.github.com>
2022-11-12 13:38:15 +0100

    new change

--- js/search.js
+++ js/search.js
@@ -16,11 +16,11 @@ function search_open(){
     let input_search=document.querySelector('.input_search');
     let loopa=document.querySelector('.loop');
 
-            if(input_search.style.width="140px"){
+            if(input_search.style.width="300px"){
 
                 input_search.style.color="white";
               
-                input_search.style.width="300px";
+                input_search.style.width="500px";
                 
                loopa.style.visibility="hidden";
              
@@ -36,10 +36,10 @@ function search_close(){
     let input_search=document.querySelector('.input_search');
     let loopa=document.querySelector('.loop');
 
-    if(input_search.style.width="300px"){
+    if(input_search.style.width="500px"){
 
         input_search.style.color= "rgb(109,109,109)";
-        input_search.style.width="140px";
+        input_search.style.width="300px";
            
 
 
